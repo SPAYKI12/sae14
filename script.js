078f4ca84d80ba8873d410210505433db934fde9
Spikey <>
2023-12-09 19:53:52 +0100

    js to change caption on image

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const burgerButton = document.querySelector('.menu-button');
     const navList = document.querySelector('.nav-list');
+    const diaporamaImage = document.querySelector('.diaporama__image');
+    const diaporamaCaption = document.querySelector('.diaporama__image-description');
+    const prevButton = document.querySelector('.diaporama__arrow-left');
+    const nextButton = document.querySelector('.diaporama__arrow-right');
 
-    burgerButton.addEventListener('click', function() {
+    burgerButton.addEventListener('click', function () {
         navList.classList.toggle('active');
         burgerButton.classList.toggle('active');
     });
 
-    var ar_images = [
+    const ar_images = [
         "diapo-images/chess-diapo.png",
         "diapo-images/python-diapo.png",
     ];
 
+    const ar_captions = [
+        "Les échecs sont l'une de mes passions",
+        "Python est l'un de mes langages de programmation préférés",
+    ];
+
     var currentImageIndex = 0;
-    var diaporamaImage = document.querySelector('.diaporama__image');
-    var prevButton = document.querySelector('.diaporama__arrow-left');
-    var nextButton = document.querySelector('.diaporama__arrow-right');
 
     function changeImage(direction) {
-        if (direction === 'prev') {
-            currentImageIndex = (currentImageIndex - 1 + ar_images.length) % ar_images.length;
-        } else if (direction === 'next') {
-            currentImageIndex = (currentImageIndex + 1) % ar_images.length;
-        }
+        currentImageIndex = (currentImageIndex + ar_images.length + (direction === 'prev' ? -1 : 1)) % ar_images.length;
 
         diaporamaImage.src = ar_images[currentImageIndex];
+        diaporamaCaption.textContent = ar_captions[currentImageIndex];
     }
 
     // Event listeners for the buttons
-    prevButton.addEventListener('click', function() {
+    prevButton.addEventListener('click', function () {
         changeImage('prev');
     });
 
-    nextButton.addEventListener('click', function() {
+    nextButton.addEventListener('click', function () {
         changeImage('next');
     });
 });
